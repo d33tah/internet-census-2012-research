36e82f6a6d9c329688fdd22fc4b54900fe7c6e1e
d33tah <d33tah@gmail.com>
2013-10-14 23:24:36 +0200

    Create a whitelist of warnings, log anything unexpected.

--- fingermatch/feeder.py
+++ fingermatch/feeder.py
@@ -9,6 +9,7 @@ it with the TCP/IP fingerprints, parsing the fingermatch's output.
 
 import subprocess
 import sys
+import re
 
 if len(sys.argv) != 2:
   usage = """Usage: %s <internetcensusfile>
@@ -17,17 +18,48 @@ if len(sys.argv) != 2:
 
 f = open(sys.argv[1])
 
+ignored_warnings = [
+  "Adjusted fingerprint due to \d+ duplicated tests",
+
+  # This one also says that it could be caused by bad network.
+  "This fingerprint contains T attributes whose value is greater than 0xFF.",
+
+  #"line is missing",
+  "(SEQ|OPS|WIN) line is missing",
+]
+
+#ignored_warnings_re = map(lambda x: re.compile(x, flags=re.MULTILINE), ignored_warnings)
+ignored_warnings_re = map(re.compile, ignored_warnings)
+
 while True:
   line = f.readline()
   if line == '':
     break
   p = subprocess.Popen(["./fingermatch", "-q", "-f", "../nmap-os-db"],
                        stdin=subprocess.PIPE,
-                       stdout=subprocess.PIPE)
-  fingerprint_column = line.split("\t")[2]
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE)
+  columns = line.split("\t")
+  fingerprint_column = columns[2]
+  ip_column = columns[0]
   fingerprint = fingerprint_column.replace(",", "\n")
+
   p.stdin.write(fingerprint)
   p.stdin.flush()
   p.stdin.close()
-  print(p.stdout.read())
+
+  program_output = p.stdout.read()
+
+  error_output = p.stderr.read()
+  if error_output != '':
+    is_ignored = False
+    for ignored_warning in ignored_warnings_re:
+      if ignored_warning.search(error_output):
+        is_ignored = True
+        break
+    if not is_ignored:
+      sys.stderr.write('Warning: %s\n' % error_output)
+      sys.stderr.flush()
+
+  print("%s\t%s" % (columns[0], program_output.strip()))
   p.terminate()
