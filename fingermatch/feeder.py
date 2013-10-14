bef88c3ffc3aa822f0369caafa57c003bdc69b6d
d33tah <d33tah@gmail.com>
2013-10-14 20:56:03 +0200

    Change feeder.py indentation to match Nmap's one.

--- fingermatch/feeder.py
+++ fingermatch/feeder.py
@@ -11,23 +11,23 @@ import subprocess
 import sys
 
 if len(sys.argv) != 2:
-    usage = """Usage: %s <internetcensusfile>
-    """ % sys.argv[0]
-    sys.exit(usage)
+  usage = """Usage: %s <internetcensusfile>
+  """ % sys.argv[0]
+  sys.exit(usage)
 
 f = open(sys.argv[1])
 
 while True:
-    line = f.readline()
-    if line == '':
-        break
-    p = subprocess.Popen(["./fingermatch", "../nmap-os-db"],
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    fingerprint_column = line.split("\t")[2]
-    fingerprint = fingerprint_column.replace(",", "\n")
-    p.stdin.write(fingerprint)
-    p.stdin.flush()
-    p.stdin.close()
-    print(p.stdout.read())
-    p.terminate()
+  line = f.readline()
+  if line == '':
+    break
+  p = subprocess.Popen(["./fingermatch", "../nmap-os-db"],
+                       stdin=subprocess.PIPE,
+                       stdout=subprocess.PIPE)
+  fingerprint_column = line.split("\t")[2]
+  fingerprint = fingerprint_column.replace(",", "\n")
+  p.stdin.write(fingerprint)
+  p.stdin.flush()
+  p.stdin.close()
+  print(p.stdout.read())
+  p.terminate()
