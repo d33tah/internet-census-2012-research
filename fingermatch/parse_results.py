30311740842df450d80f3952ee4e2bbf4a96c291
d33tah <d33tah@gmail.com>
2013-10-22 22:02:06 +0200

    Add a bit of configurability via command line to parse_results.py.

--- fingermatch/parse_results.py
+++ fingermatch/parse_results.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
 
-# parse_results.py - read the uncompressed run-research.sh output from
-# the standard input, extract and count the perfect matches and print the
-# results to the standard output.
-#
-# For a 5x speedup, run this under pypy.
+"""
+Reads the uncompressed run-research.sh output from
+the standard input, extract and count the perfect matches and print the
+results to the standard output.
+
+For a 5x speedup, run this under pypy.
+"""
 
 import sys
+import argparse
 
-# Create a dictionary where line number is a key and the fingerprint as the
-# value
-fingerprints = dict(enumerate(open('nmap-os-db').readlines()))
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--names', action='store_true',
+                    help='group by names instead of line numbers')
+args = parser.parse_args()
+
+if sys.stdin.isatty():
+  sys.exit("ERROR: %s: the script expects feeder.py output as its "
+           "standard input." % sys.argv[0])
+
+if args.names:
+  # Create a dictionary where line number is a key and the fingerprint as the
+  # value
+  fingerprints = dict(enumerate(open('nmap-os-db').readlines()))
 
 results = {}
 for line in sys.stdin:
@@ -25,13 +38,15 @@ for line in sys.stdin:
         line_number = int(match.replace('[100]', ''))
       except:
         continue
-      fingerprint_name = fingerprints[line_number-1]
-      if fingerprint_name.find("Fingerprint") == -1:
-        sys.exit(line)
-      fingerprint_name = fingerprint_name.lstrip("Fingerprint ")
-      fingerprint_name = fingerprint_name.rstrip("\r\n")
-#      key = line_number
-      key = fingerprint_name
+      if args.names:
+        fingerprint_name = fingerprints[line_number-1]
+        if fingerprint_name.find("Fingerprint") == -1:
+          sys.exit(line)
+        fingerprint_name = fingerprint_name.lstrip("Fingerprint ")
+        fingerprint_name = fingerprint_name.rstrip("\r\n")
+        key = fingerprint_name
+      else:
+        key = line_number
       if not key in results:
         results[key] = 1
       else:
