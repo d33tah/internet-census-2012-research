255fcc420374f6690e5da2db7ea85f9b17bfb4e6
d33tah <d33tah@gmail.com>
2013-10-22 17:23:48 +0200

    Group the fingerprints by their names.

--- fingermatch/pair_results.py
+++ fingermatch/pair_results.py
@@ -6,11 +6,19 @@ import sys
 # value
 fingerprints = dict(enumerate(open('nmap-os-db').readlines()))
 
+results = {}
 for line in sys.stdin.readlines():
   columns = line.split()
+  num_devices = int(columns[0])
   line_number = int(columns[1])-1
   fingerprint_name = fingerprints[line_number]
   fingerprint_name = fingerprint_name.lstrip("Fingerprint ")
   fingerprint_name = fingerprint_name.rstrip("\r\n")
-  columns += [fingerprint_name]
-  print('\t'.join(columns))
+  if not fingerprint_name in results:
+    results[fingerprint_name] = num_devices
+  else:
+    results[fingerprint_name] += num_devices
+
+results = reversed(sorted(results.iteritems(), key=lambda k: k[1]))
+for fingerprint_name, num_devices in results:
+  print("%s\t%s" % (num_devices, fingerprint_name))
