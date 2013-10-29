17e19bd25b4a176dadb328ea94556acb1ea25aa9
d33tah <d33tah@gmail.com>
2013-10-29 16:27:52 +0100

    Ask for the fingerprint line.

--- fingermatch/rematch.py
+++ fingermatch/rematch.py
@@ -12,6 +12,8 @@ import pipes
 import tempfile
 from fputils import print_stderr
 
+
+print_stderr("Enter a line from IC TCP/IP fingerprints data set.")
 ic_line = sys.stdin.readline()
 columns = ic_line.split()
 fp = columns[2]
