206beaaca511033ccdae77dca2e5801f371992e9
Jacek Wielemborek <d33tah@gmail.com>
2014-02-11 17:18:38 +0100

    Move the main routine to main().

--- servicematch/explore-format.py
+++ servicematch/explore-format.py
@@ -44,33 +44,37 @@ def run_pdb_hook(*args, **kwargs):
     traceback.print_exception(*args, **kwargs)
     pdb.pm()
 
-sys.excepthook = run_pdb_hook
 
-sizes = collections.defaultdict(int)
-outfile = open(sys.argv[2], "w")
-onebyte_file = open("by-size/1", "w")
-#for line in sys.stdin:
-for line in open(sys.argv[1]):
-    ip, timestamp, status, fp = line.split("\t")
-    ip = socket.inet_aton(ip)
-    timestamp = struct.pack("<I", int(timestamp))
-    status = chr(int(status))
-    if fp:
-        fp = decode_fp(fp)
-        #fp = zlib.compress(fp, 9)
-        sizes[len(fp)] += 1
-    fp_len = len(fp)
-    if fp_len > 1:
-        fp_md5 = md5.md5(fp).digest()
-        out = "%s%s%s%s" % (ip, timestamp, status, fp_md5)
-        outfile.write(out)
-        outfile.flush()
-        f = open("by-size/%d" % len(fp), "a")
-        f.write("%s%s" % (fp_md5, fp))
-        f.close()
-    elif fp_len == 1:
-        out = "%s%s%s%s" % (ip, timestamp, status, fp)
-        onebyte_file.write(out)
-        onebyte_file.flush()
-    else:
-        pass  # no fingerprint, do nothing for now
+def main():
+    sizes = collections.defaultdict(int)
+    outfile = open(sys.argv[2], "w")
+    onebyte_file = open("by-size/1", "w")
+    #for line in sys.stdin:
+    for line in open(sys.argv[1]):
+        ip, timestamp, status, fp = line.split("\t")
+        ip = socket.inet_aton(ip)
+        timestamp = struct.pack("<I", int(timestamp))
+        status = chr(int(status))
+        if fp:
+            fp = decode_fp(fp)
+            #fp = zlib.compress(fp, 9)
+            sizes[len(fp)] += 1
+        fp_len = len(fp)
+        if fp_len > 1:
+            fp_md5 = md5.md5(fp).digest()
+            out = "%s%s%s%s" % (ip, timestamp, status, fp_md5)
+            outfile.write(out)
+            outfile.flush()
+            f = open("by-size/%d" % len(fp), "a")
+            f.write("%s%s" % (fp_md5, fp))
+            f.close()
+        elif fp_len == 1:
+            out = "%s%s%s%s" % (ip, timestamp, status, fp)
+            onebyte_file.write(out)
+            onebyte_file.flush()
+        else:
+            pass  # no fingerprint, do nothing for now
+
+if __name__ == "__main__":
+    sys.excepthook = run_pdb_hook
+    main()
