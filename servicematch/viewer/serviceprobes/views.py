d84281ea8c72dfa019ad44029f0feea033d575ee
Jacek Wielemborek <d33tah@gmail.com>
2014-05-05 22:10:02 +0200

    Group products, times and probes.

--- servicematch/viewer/serviceprobes/views.py
+++ servicematch/viewer/serviceprobes/views.py
@@ -64,15 +64,12 @@ def one_ip(request):
     title = 'Details about IP address %s' % ip
 
     conn = psycopg2.connect(user="d33tah", port=5432, host="localhost")
-    rows = run_query(conn,
-                     """SELECT s.*,
-                          encode(s.fingerprint_md5, 'hex') fingerprint_md5,
-                          s.fingerprint, (
-                             SELECT product
-                             FROM service_probe_match m
-                             WHERE m.fingerprint_md5=s.fingerprint_md5
-                             LIMIT 1)
+    fingerprint_rows = run_query(conn,
+                     """SELECT s.*, m.*,
+                          encode(s.fingerprint_md5, 'hex') fingerprint_md5
                          FROM service_probe s
+                         JOIN service_probe_match m
+                             ON m.fingerprint_md5=s.fingerprint_md5
                          WHERE s.ip=%s""", (ip,))
 
     rdns_rows = run_query(conn, "SELECT r.rdns FROM rdns2 r WHERE r.ip=%s",
@@ -80,6 +77,31 @@ def one_ip(request):
     if len(rdns_rows) > 0:
         rdns = rdns_rows[0]['rdns']
         title += ' (%s)' % rdns
+
+    rows_dict = {}
+    for row in fingerprint_rows:
+      key = (row['portno'], row['fingerprint_md5'])
+      product_tuple = tuple(row[k] for k in ['service', 'product', 'version',
+                                             'info', 'cpe', 'os', 'hostname',
+                                             'devicetype'])
+
+      product_dict = {k: row[k] for k in ['service', 'product', 'version',
+                                          'info', 'cpe', 'os', 'hostname',
+                                          'devicetype']}
+      if key not in rows_dict:
+        rows_dict[key] = row
+        rows_dict[key]['probes'] = []
+        rows_dict[key]['times_taken'] = []
+        rows_dict[key]['products'] = []
+        rows_dict[key]['product_tuples'] = []
+
+      rows_dict[key]['probes'] += [row['probe']]
+      rows_dict[key]['times_taken'] += [row['time_taken']]
+      if not product_tuple in rows_dict[key]['product_tuples']:
+          rows_dict[key]['products'] += [product_dict]
+          rows_dict[key]['product_tuples'] += [product_tuple]
+
+    rows = rows_dict.values()
 
     return render(request, 'one_ip.html', {'rows': rows,
                                             'ip': ip,
